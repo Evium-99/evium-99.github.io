91c231ba97725da5b810b5694b061cf93d5c4367
Suave101 <57422635+Suave101@users.noreply.github.com>
2024-09-07 01:02:25 -0500

    Update

--- js/scouting.js
+++ js/scouting.js
@@ -1,4 +1,4 @@
-import { createClient } from '/@supabase/supabase-js'
+import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://cwlpwogrvudkcjesaynb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3bHB3b2dydnVka2NqZXNheW5iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU2NzU5OTAsImV4cCI6MjA0MTI1MTk5MH0.mpgEs6w4-rcDpq3A5Mq3B_0am5xGboSalhOt9a8vDpo');
